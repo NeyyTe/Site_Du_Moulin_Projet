f93d5f02fdfd1c0acd599f8bf5e4749168392821
NeyyTe <maxxence.marechal@gmail.com>
2022-11-02 15:04:37 +0100

    modifs_scroll_reveal

---
+++ js.js
@@ -0,0 +1,76 @@
+// const sr = ScrollReveal({
+//     duration: 1000,
+    
+//     // reset: true,
+// });
+
+
+
+sr.reveal('.logo', {
+    delay: 300,
+    origin:"top",
+    scale:0.8,
+});
+
+sr.reveal('.nav_links  ', {
+    duration: 1500,
+    origin: "left",
+    scale: 0.8,
+    delay: 500,
+});
+
+
+sr.reveal('.icons', {
+    duration: 2000,
+    origin: "left",
+    distance: '50px',
+    delay: 600,
+});
+
+sr.reveal('.telephone', {
+    duration: 2000,
+    origin: "left",
+    distance: '50px',
+    delay: 600,
+});
+
+sr.reveal('.titre h1', {
+    duration: 2500,
+    origin: "top",
+    distance: '50px',
+    scale: 0.9,
+    delay: 3500,
+});
+
+
+sr.reveal('.titre h2', {
+    duration: 3000,
+    origin: "top",
+    
+    scale: 0.8,
+    delay: 2000,
+});
+
+
+
+sr.reveal('.btn', {
+    duration: 3000,
+    origin: "top",
+    distance: '50px',
+    scale: 0.8,
+    delay: 5500,
+});
+
+
+sr.reveal('.articleh', {
+
+    origin: "left",
+    distance: '50px',
+    delay: 500,
+    interval:300,
+});
+
+
+
+
+
