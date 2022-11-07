d7a99dd9f9becf7641da49e940cb8819cbf7d4f8
NeyyTe <maxxence.marechal@gmail.com>
2022-11-07 16:55:40 +0100

    post_modifs_formulaire_backend

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req,res) => {
-    res.render("index.ejs")})
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
 
-app.get("/hebergement", (req,res) => {
-    res.render("hebergement.ejs")})
+app.get("/hebergement", (req, res) => {
+    res.render("hebergement.ejs")
+})
 
-    app.get("/Contact", (req,res) =>{
-        res.render("Contact.ejs")
-    })
+app.get("/Contact", (req, res) => {
+    res.render("Contact.ejs")
+})
 
-    app.listen(8080);
+app.post("/", (req, res) => {
+    res.send("Nom :" + req.body.lastname + "<br>"  + "Prénom : " + req.body.firstname + "<br>"  + "Adresse : " + req.body.street + "<br>"  + "Code postal: " + req.body.zipCode + "<br>"  + "Ton Prénom est : " + req.body.city + "<br>"  + "Date de naissance : " + req.body.birthdate + "<br>"  + "Message : " + req.body.description)
+})
+
+app.listen(8080); () => {
+    console.log("Serveur lancé deans le port 8080")
+}
 
